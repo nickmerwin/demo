d1d97c5d308767d7ae21913d1427d6a36ae60165
Nick Merwin <n@mer.io>
2017-01-24 15:50:24 -0800

    unskip

--- spec/run_spec.rb
+++ spec/run_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe Run do
   it "runs" do
     expect(Run.this_is_covered).to eq 1
-    # expect(Run.another_method).to eq "yo"
+    expect(Run.another_method).to eq "yo"
   end
 end
